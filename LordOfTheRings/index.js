d1fc0d79a0824d7c6c14ae2fe58c62ce7427a316
Akintobi41 <akintobi41moyinoluwa@gmail.com>
2022-09-06 17:52:45 +0100

    updated homepage and styles

---
+++ LordOfTheRings/index.js
@@ -0,0 +1,16 @@
+let hamToggle = document.querySelector(".hamburger");
+let navigation = document.querySelector(".navigation");
+
+document.onclick = function (e) {
+    if (e.target !== hamToggle && e.target !== navigation) {
+        hamToggle.classList.remove("active");
+        navigation.classList.remove("active");
+    }
+};
+
+hamToggle.onclick = function () {
+    hamToggle.classList.toggle("active");
+    navigation.classList.toggle("active");
+
+};
+
